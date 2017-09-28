f1fc006683b60e0e0c532192fbbedb58daf98f67
rockboynton <32227280+rockboynton@users.noreply.github.com>
2017-09-28 14:05:44 -0500

    Add files via upload

--- Lab4.java
+++ Lab4.java
@@ -1,3 +1,11 @@
+/*
+ * SE1011-011
+ * Fall 2017
+ * Assignment: Lab 4
+ * Author: Rock Boynton
+ * Created: 9/28/2017
+ */
+
 package boyntonrl;
 
 import java.util.*;
@@ -5,83 +13,70 @@ import java.lang.*;
 
 public class Lab4 {
     public static void main(String args []) {
+        /*
+         * This is program runs a game where the user has an uncle close to death and they get to choose between
+         * receiving an their inheritance in one of two ways:
+         * 1. Starting with a given amount (such as $5000), he will add that amount to their total inheritance every week, or
+         * 2. Starting with one penny, he will double their inheritance every week.
+         * Option 2 will always outpace option 1 at some point in the future, but the game is when picking which will be
+         * a better financial decision for you... will option 2 yield more before your uncle dies?
+         */
 
-//        int WEEKS_LEFT_MAX = 40;
-        int WEEKS_LEFT;
-//        int STARTING_AMOUNT_MAX = 5000;
-        double STARTING_AMOUNT_LINEAR;
-//        System.out.println(Math.random() + "AND" + STARTING_AMOUNT_LINEAR);
+        int WEEKS_TILL_DEATH_MAX = 40;
+        int LINEAR_STARTING_AMOUNT_MAX = 5000;
+        double ONE_CENT = 0.01;
 
         Scanner input = new Scanner(System.in);
+
+        double startingAmountLinear;
         double linearTotal;
         double exponentialTotal;
         double inheritanceDifference;
-        double weeksLeftLoop;
+        int weeksTillDeath;
+        int weeksLeftLoop;
         int weeks;
         int playAgain;
         int choice;
 
         do {
-            WEEKS_LEFT = (int) (Math.random() * 40);
-            STARTING_AMOUNT_LINEAR = (Math.random() * 5000);
-            linearTotal = STARTING_AMOUNT_LINEAR;
-            exponentialTotal  = 0.01;
-            weeksLeftLoop = WEEKS_LEFT;
+            weeksTillDeath = (int) (Math.random() * WEEKS_TILL_DEATH_MAX); // assigning var to a random value between 0 and WEEKS_TILL_DEATH_MAX
+            startingAmountLinear = (Math.random() * LINEAR_STARTING_AMOUNT_MAX); // assigning var to a random value between 0 and LINEAR_STARTING_AMOUNT_MAX
+            linearTotal = startingAmountLinear;
+            exponentialTotal  = ONE_CENT;
+            weeksLeftLoop = weeksTillDeath - 1; // subtract 1 so it doesn't print the week the uncle dies
             weeks = 1;
             System.out.print("Suppose you have a wealthy uncle who gives you two options for your inheritance. He will" +
                     " add to your inheritance every weeks until he dies according to one of two schemes:\n" +
                     "1. Starting with a given amount ($0-5000), he will add that amount to your total inheritance every weeks, or\n" +
                     "2. Starting with one penny, he will double your inheritance every weeks.\n" +
                     "Enter which option you would prefer: ");
-            choice = input.nextInt();
-            while (exponentialTotal < linearTotal && weeksLeftLoop > 0) {
-                linearTotal += STARTING_AMOUNT_LINEAR;
-                exponentialTotal *= 2;
-                System.out.println("Week " + weeks + "  Linear: $" + linearTotal + ", Exponential: $" + exponentialTotal);
-                weeks++;
-                weeksLeftLoop--;
+            choice = input.nextInt(); //determines whether user choose exponential or linear
+            while (exponentialTotal < linearTotal && weeksLeftLoop > 0) { //loop runs until either exponential total surpasses linear total or their uncle dies
+                System.out.format("Week %2d Linear: $%,.2f, Exponential: $%,.2f\n", weeks, linearTotal, exponentialTotal);
+                linearTotal += startingAmountLinear;
+                exponentialTotal *= 2; //doubles every week
+                weeks++; //increment weeks number that is display
+                weeksLeftLoop--; //decrement weeks left in loop  end while loop
             }
-            if (weeksLeftLoop > 0){
-                for (int weeksLeft = (int) weeksLeftLoop; weeksLeft > 0; weeksLeft--){
+            if (weeksLeftLoop > 0){ //if the exponential total already surpassed the linear total...
+                System.out.format("Week %2d Linear: $%,.2f, Exponential: $%,.2f\n", weeks, linearTotal, exponentialTotal);
+                for (int weeksLeft = weeksLeftLoop; weeksLeft > 0; weeksLeft--){ //this calculates the exponential total for the rest of the weeks until the uncle dies
                     exponentialTotal *= 2;
                 }
-            }
+            } //end if statement
             System.out.println();
             inheritanceDifference = Math.abs(linearTotal - exponentialTotal);
-            if ((choice == 1 && linearTotal > exponentialTotal) || (choice == 0 && linearTotal < exponentialTotal)) {
-                System.out.print("Your uncle dies after " + WEEKS_LEFT + " weeks so you got lucky and ended up with an extra $" +
-                        inheritanceDifference);
-            } else {
-                System.out.print("Your uncle dies after " + WEEKS_LEFT + " weeks so you got unlucky and missed out on an extra $" +
-                        inheritanceDifference);
-            }
-            System.out.println();
+            if ((choice == 1 && linearTotal > exponentialTotal) || (choice == 2 && linearTotal < exponentialTotal)) { //if the user "won"
+                System.out.format("Your uncle dies after %2d weeks so you got lucky and " +
+                        "ended up with an extra $%,.2f", weeksTillDeath, inheritanceDifference);
+            } else { // if the user "lost"
+                System.out.format("Your uncle dies after %2d weeks so you got unlucky and " +
+                        "missed out on an extra $%,.2f", weeksTillDeath, inheritanceDifference);
+            } //end if statement
+            System.out.println(); //skip line
             System.out.println("Do you want to play again? (1 for yes) or (0 for no): ");
             playAgain = input.nextInt();
-            System.out.println();
-        } while (playAgain == 1);
-//        System.out.format("Week %2d Linear: $%.2f\n", numberOfWeeks, linearAmount);
-    }
-}
-/*
- *  Suppose you have a wealthy uncle who gives you two options for your inheritance. He will add to your inheritance
- *  every week until he dies according to one of two schemes:
- *
- *  Starting with a given amount (such as $5000), he will add that amount to your total inheritance every week, or
- *  Starting with one penny, he will double your inheritance every week.
- *  Option 2 will always outpace option 1 at some point in the future, but the question is when will option 2 be a better
- *  financial decision for you than option 1 and will it happen before your uncle dies?
- *
- *  For this lab you will be creating a game that explains the options briefly and allows a user to pick option 1 or option
-  *  2. The starting amount for option 1 should be randomly generated (such as between $0 and $5000) each time the user plays the game.
-  *  Also the number of remaining weeks of life for your uncle should be randomly generated (such as between 0 and 40 weeks).
- *
- *  You should display the value of the inheritance for each option, each week until either:
- *
- *  Option 2 exceeds option 1, or
- *  Your uncle dies
- *  You should tell the user whether they “won” or not and how much they won or lost. They win if they chose the option
- *  that causes them to end up with more money. They lose if they chose the option that leaves them with less money.
- *
- *  Lastly, you should give the user the option of playing the game again every time they see their results.
- */
+            System.out.println(); //skip line
+        } while (playAgain == 1); //determines if the user wants to play again, otherwise end do while loop
+    } //end main
+} // end class Lab4
